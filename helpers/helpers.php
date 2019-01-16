490acea90abddb4f6bf6a31aa1201573db79afe6
Harun R Rayhan <harun.b13@gmail.com>
2019-01-16 18:11:19 +0600

    A beta version

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,11 +1,39 @@
 <?php
-
+/**
+ * @package WC_Add_To_Cart_Redirection
+ */
 // Compatibility Check
-function amz_wc_add_to_cart_redirection_compatibility_check()
+function amz_wcrd_check_woocommerce_plugin()
 {
 	if ( ! class_exists( 'WooCommerce' ) ) {
 		return __( 'Please install and activate <strong>WooCommerce</strong> plugin first.', 'amz_wcrd' );
 	}
 
+	if ( version_compare( wc()->version, AMZ_WCRD_MINIMUM_WOOCOMMERCE_VERSION, '<=' ) ) {
+		return sprintf( __( "Please update your WooCommerce plugin. It's outdated. %s or latest required", 'amz_wcrd' ), AMZ_WCRD_MINIMUM_WOOCOMMERCE_VERSION );
+	}
+
 	return false;
+}
+
+// Plugin Die Message
+function amz_wcrd_plugin_die_message( $message )
+{
+	return '<p>' .
+	       $message
+	       . '</p> <a href="' . admin_url( 'plugins.php' ) . '">' . __( 'Go back', 'amz_wcrd' ) . '</a>';
+}
+
+function dd($code){
+	echo '<pre>';
+	var_dump($code);
+	echo '</pre>';
+	wp_die();
+}
+
+function dp($code){
+	echo '<pre>';
+	print_r($code);
+	echo '</pre>';
+	wp_die();
 }
--- wc-add-to-cart-redirection.php
+++ wc-add-to-cart-redirection.php
@@ -12,7 +12,7 @@
  * Requires at least:       4.5
  * Tested up to:            5.0.3
  * WC requires at least:    2.7
- * WC tested up to:         3.3
+ * WC tested up to:         3.5.3
  *
  * License:                 GPLv3
  * License URI:             http://www.gnu.org/licenses/gpl-3.0.html
@@ -25,21 +25,49 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define AMZ_WC_REDIRECTION_FILE
-if ( ! defined( 'AMZ_WC_REDIRECTION_FILE' ) ) {
-	define( 'AMZ_WC_REDIRECTION_FILE', __FILE__ );
+if ( ! defined( 'AMZ_WCRD_REDIRECTION_FILE' ) ) {
+	define( 'AMZ_WCRD_REDIRECTION_FILE', __FILE__ );
+}
+
+// Define Minimum WooCommerce Version
+if ( ! defined( 'AMZ_WCRD_MINIMUM_WOOCOMMERCE_VERSION' ) ) {
+	define( 'AMZ_WCRD_MINIMUM_WOOCOMMERCE_VERSION', 2.7 );
+}
+
+// Plugin DIRNAME
+if ( ! defined( 'AMZ_WCRD_PLUGIN_DIRNAME' ) ) {
+	define( 'AMZ_WCRD_PLUGIN_DIRNAME', dirname( plugin_basename( __FILE__ ) ) );
+}
+
+// Plugin Version
+if ( ! defined( 'AMZ_WCRD_PLUGIN_VERSION' ) ) {
+	define( 'AMZ_WCRD_PLUGIN_VERSION', '0.1.0' );
 }
 
 // Helper Functions
-require_once('helpers/helpers.php');
+require_once( dirname( __FILE__ ) . '/helpers/helpers.php' );
 
 // Register Activation Hook
-register_activation_hook( __FILE, function () {
+register_activation_hook( __FILE__, function () {
 	// Check if WooCommerce installed or Compatible
-	$notice = amz_wc_add_to_cart_redirection_compatibility_check();
-	if($notice) {
-
+	$notice = amz_wcrd_check_woocommerce_plugin();
+	if ( $notice ) {
+		deactivate_plugins( basename( __FILE__ ) );
+		wp_die( amz_wcrd_plugin_die_message( $notice ) );
 	}
 } );
+
+// Add Class for Redirection
+if ( ! class_exists( 'AMZ_WooCommerce_Add_To_Cart_Redirection' ) ) {
+	require_once( dirname( __FILE__ ) . '/includes/class.AMZ_WooCommerce_Add_To_Cart_Redirection.php' );
+}
+
+
+// Add AMZ_WooCommerce_Add_To_Cart_Redirection Class to Loaded hook
+function amz_wcr_instance() {
+	return AMZ_WooCommerce_Add_To_Cart_Redirection::instance();
+}
+add_action( 'plugins_loaded', 'amz_wcr_instance' );
 
 
 
